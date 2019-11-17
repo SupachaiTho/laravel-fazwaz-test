fb48844807b9aa6bbc2305a01ebba5351ea1768b
Supachai.tho <supachai.tho@student.mahidol.ac.th>
2019-11-17 18:55:45 +0700

    Prepare enviroment for Unit Test model

--- tests/Unit/SeederTest.php
+++ tests/Unit/SeederTest.php
@@ -5,19 +5,37 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
 use App\Property;
 use App\PropertyType;
 use App\Status;
 use App\Region;
 use App\Project;
 
-class PropertyTest extends TestCase
+class SeederTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * A basic unit test example.
      *
      * @return void
      */
+
+    // How To Test State 3
+    // 1. REMOVE line 34 and 35 at phpunit.xml
+    // 2. REMOVE protected function setUp at SeederTest.php
+    // 3. run `php artisan migrate:fresh --seed`
+    // 4. run `vendor/bin/phpunit`
+    
+    protected function setUp(): void 
+    {
+
+        $this->markTestSkipped('Test State 3');
+    }
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
 
     // Total properties are 100,000
     public function test_have_100000_properties()
@@ -43,5 +61,22 @@ class PropertyTest extends TestCase
         ->where('for_rent', true)
         ->where('region_id', 4)
         ->count());
+    }
+
+    // Total projects are 10,000
+    public function test_have_10000_projects()
+    {
+        $this->assertEquals(10000, Project::count());
+    }
+
+    // There should be 1 project with 2001 properties
+    public function test_have_2001_properties()
+    {
+        $this->assertEquals(1, count(\DB::select('
+            SELECT project_id, count(property_id) as num
+            FROM properties
+            GROUP BY project_id
+            HAVING num = 2001')
+        ));
     }
 }
