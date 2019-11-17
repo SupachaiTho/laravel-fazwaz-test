c7d20e93a0a6920bfa6db620e9faad8c4bca91aa
Supachai.tho <supachai.tho@student.mahidol.ac.th>
2019-11-18 02:02:25 +0700

    a

--- database/seeds/PropertiesTableSeeder.php
+++ database/seeds/PropertiesTableSeeder.php
@@ -13,17 +13,19 @@ class PropertiesTableSeeder extends Seeder
     {
         \DB::table('properties')->delete();
 
-        factory(App\Property::class, 2001)->states('first_project')->create();
-        factory(App\Property::class, 3000)->states('active_condo')->create();
-        for ($i = 0; $i < 100; $i++) {
-            factory(App\Property::class, 940)->create();
-        }
-        factory(App\Property::class, 999)->create();
+        factory(App\Property::class, 100)->create();
 
-        DB::table('properties')
-        ->where('status_id', 2)
-        ->where('property_type_id', 2)
-        ->where('for_rent', true)
-        ->update(['region_id' => App\Region::first()->id]);
+        // factory(App\Property::class, 2001)->states('first_project')->create();
+        // factory(App\Property::class, 3000)->states('active_condo')->create();
+        // for ($i = 0; $i < 100; $i++) {
+        //     factory(App\Property::class, 940)->create();
+        // }
+        // factory(App\Property::class, 999)->create();
+
+        // DB::table('properties')
+        // ->where('status_id', 2)
+        // ->where('property_type_id', 2)
+        // ->where('for_rent', true)
+        // ->update(['region_id' => App\Region::first()->id]);
     }
 }
